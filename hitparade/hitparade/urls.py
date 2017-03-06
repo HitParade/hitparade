a5395e42e219d7b88e3264325aa7730601d96f5f
Chris Henry <henry.christopher@gmail.com>
2017-03-06 16:18:02 -0500

    chg: install social starter app

--- hitparade/hitparade/urls.py
+++ hitparade/hitparade/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
+    url(r"^account/social/accounts/$", TemplateView.as_view(template_name="account/social.html"), name="account_social_accounts"),
+    url(r"^account/social/", include("social_django.urls", namespace="social")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
