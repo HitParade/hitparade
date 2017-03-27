f7a1e07386544f073d2279f9a043d85bba029c8b
Chris Henry <henry.christopher@gmail.com>
2017-03-27 12:42:18 -0400

    new: Add flask app and configure connection to postgres

---
+++ data/app/config.py
@@ -0,0 +1,41 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s env variable" % var_name
+        raise Exception(error_msg)
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_DATABASE_URI = "postgres://" + \
+        get_env_variable('POSTGRES_USER') + ":" + \
+        get_env_variable('POSTGRES_PASSWORD') + "@" + \
+        get_env_variable('POSTGRES_HOST') + "/" + \
+        get_env_variable('POSTGRES_DB')
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
--- data/app/main.py
+++ data/app/main.py
@@ -1,8 +1,18 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 @app.route("/")
 def hello():
+
+    print app.config
+
     return "Hello World from HitParade Flask in a uWSGI Nginx Docker container with \
      Python 2.7"
 
