f5c15b6aea9ea0bdad6b445de18b319218a8e5e8
Chris Henry <henry.christopher@gmail.com>
2017-04-13 12:30:42 -0400

    new: start of the Team model

--- data/app/main.py
+++ data/app/main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
+from models import Team
+
 @app.route("/")
 def hello():
 
--- data/app/models.py
+++ data/app/models.py
@@ -2,3 +2,21 @@ from main import db
 from sqlalchemy.dialects.postgresql import JSON
 
 
+class Team(db.Model):
+    __tablename__ = 'team'
+    __name__ = 'Team'
+
+    id = db.Column(db.Integer, primary_key=True)
+    abbreviation = db.Column(db.String(3), unique=True, index=True)
+    active = db.Column(db.Boolean())
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    conference = db.Column(db.String(32))
+    division = db.Column(db.String(12))
+    site_name = db.Column(db.String(64))
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(32))
+    full_name = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<{} {}>'.format(self.__name__, self.abbreviation)
