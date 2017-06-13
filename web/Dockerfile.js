24d372042db0c445841ad1432c9daf9298e98af1
Zak Stallings <zakstal@gmail.com>
2017-06-12 20:18:09 -0500

    add npm start to docker-compose.yml and add build and watch to npm start in package.json. added watch to npm run build which may not be the best thing to do if the build script is going to be run at any other time then development. removed watchers for js and css as that is being taken car of by webpack

--- web/Dockerfile.js
+++ web/Dockerfile.js
@@ -3,8 +3,10 @@ FROM node:7.10.0-alpine
 RUN mkdir -p /code/
 
 ADD hitparade/ /code/
+
+ADD package.json ./
 WORKDIR /code
 
 RUN npm install
 
-CMD ["/usr/local/bin/npm run watch"]
+CMD ["/usr/local/bin/npm run start"]
