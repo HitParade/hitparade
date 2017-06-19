c460f4088a0b7bead398322a18b4be3acdbf2458
Chris Henry <henry.christopher@gmail.com>
2017-06-19 12:13:17 -0400

    new: Quick script to load all initial data

---
+++ web/hitparade/hitparade/management/commands/load-initial.py
@@ -0,0 +1,23 @@
+import pprint
+
+from django.conf import settings
+from django.core.management import call_command, base
+
+from hitparade.models import Conference, Division, Team
+from hitparade.utils import get_stattleship_client, move_ssid
+
+
+class Command(base.BaseCommand):
+    help = 'Loads initial data'
+
+
+    def handle(self, *args, **options):
+
+        print "Load All the Thingz!!!!1!"
+
+        call_command('load-teams')
+        call_command('load-players')
+        call_command('load-games')
+        call_command('load-bis-historical')
+        call_command('load-bis-daily', '--date 20170301')
+        call_command('load-atbats')
