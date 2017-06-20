db4cad2fc239f3035b3d364133a8c1d850bbc370
Karl Hoglander <karl@Matrixofts-MacBook-Pro.local>
2017-06-19 20:23:11 -0400

    Updated lineup loading based on feedback from Chris, and some further tweaks by me

--- web/hitparade/hitparade/management/commands/load-lineups.py
+++ web/hitparade/hitparade/management/commands/load-lineups.py
@@ -5,58 +5,45 @@ from bs4 import BeautifulSoup
 
 from django.core.management.base import BaseCommand
 
-def findNpop(match, data, prop, select, contentIndex, arrayIndex = 0):
+def findNpop(data, select, contentIndex, arrayIndex = 0):
     item = data.select(select)
-    match[prop] = item[arrayIndex].contents[contentIndex].strip()
+
+    return item[arrayIndex].contents[contentIndex].strip()
 
 def findlineup(htmlLineup, select):
     lineup = []
 
     for htmlPlayer in htmlLineup.select(select):
-        player = {}
-
-        findNpop(player, htmlPlayer, "Pos", '.dlineups-pos', 0)
-        findNpop(player, htmlPlayer, "Name", 'a', 0)
-        findNpop(player, htmlPlayer, "LR", '.dlineups-lr', 0)
-
-        lineup.append(player)
+        lineup.append({
+            "Pos": findNpop(htmlPlayer, '.dlineups-pos', 0),
+            "Name": findNpop(htmlPlayer, 'a', 0),
+            "LR": findNpop(htmlPlayer, '.dlineups-lr', 0)
+        })
     
     return lineup
 
 class Command(BaseCommand):
     help = 'Grabs lineup data from Rotowire.'
 
-    def handle(self, *args, **options):
+    def createTeam(self, htmlMatch):
+        return {
+            "AwayTeam": findNpop(htmlMatch, "div.dlineups-topboxleft", 0),
+            "HomeTeam": findNpop(htmlMatch, "div.dlineups-topboxright", 2),
+            "Time": findNpop(htmlMatch, "div.dlineups-topboxcenter-topline a", 0),
+            "Weather": findNpop(htmlMatch, "div.dlineups-topboxcenter-bottomline", 0),
+            "AwayPitcher": findNpop(htmlMatch, "div.dlineups-pitchers a", 0),
+            "HomePitcher": findNpop(htmlMatch, "div.dlineups-pitchers a", 0, 1),
+            "AwayLineup": findlineup(htmlMatch.select('.dlineups-half')[0], '.dlineups-vplayer'),
+            "HomeLineup": findlineup(htmlMatch.select('.dlineups-half')[1], '.dlineups-hplayer')
+        }
 
+    def handle(self, *args, **options):
         r = requests.get('http://www.rotowire.com/baseball/daily_lineups.htm')
 
         soup = BeautifulSoup(r.text, 'html.parser')
 
-        htmlTops = soup.select("div.dlineups-topbox")
+        htmlMatches = [top.parent for top in soup.select("div.dlineups-topbox")]
 
-        htmlMatches = []
+        matchups = [self.createTeam(htmlMatch) for htmlMatch in htmlMatches]
 
-        for top in htmlTops:
-            htmlMatches.append(top.parent)
-
-        count = len(htmlMatches)
-
-        matchups = []
-
-        for htmlMatch in htmlMatches:
-            match = {}
-
-            findNpop(match, htmlMatch, "AwayTeam", "div.dlineups-topboxleft", 0)
-            findNpop(match, htmlMatch, "HomeTeam", "div.dlineups-topboxright", 2)
-            findNpop(match, htmlMatch, "Time", "div.dlineups-topboxcenter-topline a", 0)
-            findNpop(match, htmlMatch, "Weather", "div.dlineups-topboxcenter-bottomline", 0)
-            findNpop(match, htmlMatch, "AwayPitcher", "div.dlineups-pitchers a", 0)
-            findNpop(match, htmlMatch, "HomePitcher", "div.dlineups-pitchers a", 0, 1)
-
-            match["AwayLineup"] = findlineup(htmlMatch.select('.dlineups-half')[0], '.dlineups-vplayer')
-            match["HomeLineup"] = findlineup(htmlMatch.select('.dlineups-half')[1], '.dlineups-hplayer')
-
-            matchups.append(match)
-
-        for item in matchups:
-            pprint.pprint(item)
+        pprint.pprint(matchups)
