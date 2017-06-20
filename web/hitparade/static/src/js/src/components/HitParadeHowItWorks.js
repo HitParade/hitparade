b962e2d5b4522f9651aeb21bef9389d6c6201cd9
Zak Stallings <zakstal@gmail.com>
2017-06-20 17:33:33 -0500

    remove old code

--- web/hitparade/static/src/js/src/components/HitParadeHowItWorks.js
+++ web/hitparade/static/src/js/src/components/HitParadeHowItWorks.js
@@ -71,81 +71,18 @@ const HitParadeHowItWorks = props => {
                         <section className="hp-why-header-divider">
                             <img src={props.imgRoot+'divider.svg'}  className="hp-divider" />
                         </section>
-                        <section
-                              className="how-it-works-steps-container"
-                        >
-                              {
-                                    _.map(howItWoksSteps, (panel, i) => 
-                                          (<HowItWorksStepsPanel 
-                                                key={panel.title}
-                                                reverse={i % 2 !== 0}
-                                                { ...panel }
-                                          />))
-                              }
-                        </section>
-                        {/*<div className="hp-panel-row-container">
-                        <div 
-                              className="hp-panel-row-end"  
-                              data-170-center="opacity: 1;transform: translateX( 0% ); "
-                              data-200-center="opacity: 1;transform: translateX( 50% );  "
-                              data-220-center="opacity: 0.01;transform: translateX( 100% );  "
-                        >
-
-                            <div className="hp-panel-relative">
-                                  <div className="hp-img-panel">
-                                        <img src={props.imgRoot + 'howItWorks1.svg'}  />
-                                  </div>
-                                  <div className="hp-panel-text">
-                                        <div className="hp-panel-text-h1 niveau-grotesk-black"><img className="hp-diamond-bullet" src={props.imgRoot+'diamondBullet.svg'} /><span className="h1-move">STEP 1:</span></div>
-                                        <div className="hp-panel-text-desc proxima-nova-regular">Choose a player from our database.</div>
-                                  </div>
-                            </div>
-                           <div className="hp-rectangle"></div>
-
-                      </div>
-                        </div>*/}
-                       {/*<div className="hp-panel-row-container">
-                              <div 
-                                    className="hp-panel-row-start"  
-                                    data-170-center="opacity: 1;transform: translateX( 0% ); "
-                                    data-200-center="opacity: 1;transform: translateX( -50% );  "
-                                    data-220-center="opacity: 0.01;transform: translateX( -100% );  "
+                              <section
+                                    className="how-it-works-steps-container"
                               >
-                            <div className="hp-panel-relative">
-                                  <div className="hp-panel-text">
-                                        <div className="hp-panel-text-h1 niveau-grotesk-black"><img className="hp-diamond-bullet" src={props.imgRoot+'diamondBullet.svg'}  /><span className="h1-move">STEP 2:</span></div>
-                                        <div className="hp-panel-text-desc proxima-nova-regular">View detailed analytics, stats for all of the chosen players history and view our predictions as to whether the player will get a hit. </div>
-                                  </div>
-                                  <div className="hp-img-panel">
-                                        <img src={props.imgRoot + 'howItWorks2.svg'} />
-                                  </div>
-                            </div>
-                            <div className="hp-rectangle-ls"></div>
-                      </div>
-                       </div>*/}
-                      {/*<div className="hp-panel-row-container">
-                      <div 
-                        className="hp-panel-row-end" 
-                        style={{marginBottom: '250px'}}  
-                        data-170-center="opacity: 1;transform: translateX( 0% ); "
-                        data-200-center="opacity: 1;transform: translateX( 50% );  "
-                        data-220-center="opacity: 0.01;transform: translateX( 100% );  "
-                        >
-
-
-                            <div className="hp-panel-relative">
-
-                                  <div className="hp-img-panel">
-                                        <img src={props.imgRoot + 'howItWorks3.svg'}  />
-                                  </div>
-                                  <div className="hp-panel-text">
-                                        <div className="hp-panel-text-h1 niveau-grotesk-black"><img className="hp-diamond-bullet" src={props.imgRoot+'diamondBullet.svg'}  /><span className="h1-move">STEP 3:</span></div>
-                                        <div className="hp-panel-text-desc proxima-nova-regular">Choose the most probable player(s) to get a hit to Beat the Streak and WIN $5.6 million! </div>
-                                  </div>
-                            </div>
-                            <div className="hp-rectangle"></div>
-                      </div>
-                      </div>*/}
+                                    {
+                                          _.map(howItWoksSteps, (panel, i) => 
+                                                (<HowItWorksStepsPanel 
+                                                      key={panel.title}
+                                                      reverse={i % 2 !== 0}
+                                                      { ...panel }
+                                                />))
+                                    }
+                              </section>
                         </section>
 
 
