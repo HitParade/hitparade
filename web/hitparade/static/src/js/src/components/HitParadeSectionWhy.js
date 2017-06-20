cac7eba6d169320a0c0bf03ed43d71ea6c89a0f4
Zak Stallings <zakstal@gmail.com>
2017-06-20 11:17:23 -0500

    adjust scss for why section

--- web/hitparade/static/src/js/src/components/HitParadeSectionWhy.js
+++ web/hitparade/static/src/js/src/components/HitParadeSectionWhy.js
@@ -17,18 +17,16 @@ const HitParadeSectionWhy = props => {
 							src={props.imgRoot +'diamond.svg'}
 						/>
 					
-						<div className="hp-why-coin-image-container"
-								data-170-center="opacity: 1;transform: scale( 1.0 ); "
-								data-200-center="opacity: 1;transform: scale( 1.3 ); "
-								data-220-center="opacity: 0.01; transform:  scale( 0.1 ); ">
-							<img src={props.imgRoot +'coins.svg'}  className="hp-why-coin-image" />
-						</div>
-						<section className="hp-why-main-content">
+						<div className="hp-why-container">
+							<div className="hp-why-coin-image-container"
+									data-170-center="opacity: 1;transform: scale( 1.0 ); "
+									data-200-center="opacity: 1;transform: scale( 1.3 ); "
+									data-220-center="opacity: 0.01; transform:  scale( 0.1 ); ">
+								<img src={props.imgRoot +'coins.svg'}  className="hp-why-coin-image" />
+							</div>
+							<section className="hp-why-main-content">
 								<section className="hp-why-header-top niveau-grotesk-black">
 									BEAT THE STREAK FOR THE COST OF YOUR MORNING COFFEE
-								</section>
-								<section className="hp-why-header-divider">
-									<img src={props.imgRoot +'divider.svg'}   className="hp-divider" />
 								</section>
 								<section className="hp-why-header-bottom">
 									<section className="hp-why-header-bottom-text proxima-nova-regular">
@@ -39,11 +37,17 @@ const HitParadeSectionWhy = props => {
 												<section className="hp-why-register-text  niveau-grotesk-black">REGISTER FOR EARLY ACCESS</section>
 												<section className="hp-why-register-text-desc proxima-nova-regular">Get Free Access for 1 week.</section>
 										</section>
-										<section className="hp-why-register-button"><HitParadeButton className="hp-why-register-button niveau-grotesk-black " buttonText="REGISTER TODAY"  clickMethod={props.navs.click.navSignup} /></section>
+										<section className="hp-why-header-bottom-panel-right">
+											<HitParadeButton 
+												className="hp-why-register-button niveau-grotesk-black " 
+												buttonText="REGISTER TODAY"  
+												clickMethod={props.navs.click.navSignup} 
+											/>
+										</section>
 									</section>
-
 								</section>
-						</section>
+							</section>
+						</div>
 					<Diamonds 
 						className="hp-section-why-footer"
 						count="11" 
