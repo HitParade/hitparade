11f79593600338926015263427237ceb423930a0
Karl Hoglander <karl@Matrixofts-MacBook-Pro.local>
2017-06-21 21:57:57 -0400

    Updated lineup command based on feedback

--- web/hitparade/hitparade/management/commands/load-lineups.py
+++ web/hitparade/hitparade/management/commands/load-lineups.py
@@ -21,11 +21,12 @@ def findNpopTitle(data, select, contentIndex, arrayIndex = 0):
 def findlineup(htmlLineup, select):
     lineup = []
 
-    for htmlPlayer in htmlLineup.select(select):
+    for index, item in enumerate(htmlLineup.select(select)):
         lineup.append({
-            "Pos": findNpop(htmlPlayer, '.dlineups-pos', 0),
-            "Name": findNpopTitle(htmlPlayer, 'a', 0),
-            "LR": findNpop(htmlPlayer, '.dlineups-lr', 0)
+            "Pos": findNpop(item, '.dlineups-pos', 0),
+            "Name": findNpopTitle(item, 'a', 0),
+            "LR": findNpop(item, '.dlineups-lr', 0),
+            "Order": index
         })
     
     return lineup
@@ -37,7 +38,7 @@ class Command(BaseCommand):
     help = 'Grabs lineup data from Rotowire.'
 
     def createTeam(self, htmlMatch):
-        ump =findLinkWithHref(htmlMatch, '/daily/mlb/umpire_stats.php')
+        ump = findLinkWithHref(htmlMatch, '/daily/mlb/umpire_stats.php')
 
         return {
             "AwayTeam": findNpop(htmlMatch, "div.dlineups-topboxleft", 0),
@@ -48,7 +49,7 @@ class Command(BaseCommand):
             "HomePitcher": findNpop(htmlMatch, "div.dlineups-pitchers a", 0, 1),
             "AwayLineup": findlineup(htmlMatch.select('.dlineups-half')[0], '.dlineups-vplayer'),
             "HomeLineup": findlineup(htmlMatch.select('.dlineups-half')[1], '.dlineups-hplayer'),
-            "Ump": '' if not len(ump) else ump[0].text
+            "Official": None if not len(ump) else ump[0].text
         }
 
     def handle(self, *args, **options):
@@ -62,24 +63,25 @@ class Command(BaseCommand):
 
         matchups = [self.createTeam(htmlMatch) for htmlMatch in htmlMatches]
 
-        #pprint.pprint(matchups)
-
         for matchup in matchups:
-            matchup["throwaway"], matchup["Away"] = GameStat.get_team_ref('code', matchup["AwayTeam"])
-            matchup["throwaway"], matchup["Home"] = GameStat.get_team_ref('code', matchup["HomeTeam"])
-            matchup["throwaway"], matchup["Off"] = GameStat.get_umpire_ref('name', matchup["Ump"])
+            matchup["Away"] = GameStat.get_team_ref('code', matchup["AwayTeam"])[1]
+            matchup["Home"] = GameStat.get_team_ref('code', matchup["HomeTeam"])[1]
+            matchup["Official"] = GameStat.get_umpire_ref('name', matchup["Official"])[1]
 
             for person in matchup["AwayLineup"]:
-                matchup["throwaway"], person["Player"] = GameStat.get_player_ref('player', person["Name"])
+                person["Player"] = GameStat.get_player_ref('player', person["Name"])[1]
 
             for person in matchup["HomeLineup"]:
-                matchup["throwaway"], person["Player"] = GameStat.get_player_ref('player', person["Name"])
+                person["Player"] = GameStat.get_player_ref('player', person["Name"])[1]
 
-            game = Game.objects.filter(season = now.year
-                ,on = now.strftime("on %B %d, %Y")
-                ,home_team = matchup["Home"]
-                ,away_team = matchup["Away"])
+            game = Game.objects.filter(
+                season = now.year,
+                on = now.strftime("on %B %d, %Y"),
+                home_team = matchup["Home"],
+                away_team = matchup["Away"])
 
             pprint.pprint(now)
             pprint.pprint(now.strftime("on %B %d, %Y"))
             #pprint.pprint(game)
+        
+        pprint.pprint(matchups)
