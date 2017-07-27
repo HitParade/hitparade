f01379c6e24bb91ecba83b01629dce6b3a46fae4
Zak Stallings <zakstal@gmail.com>
2017-07-26 21:33:42 -0500

    remove test for emails

--- web/hitparade/static/src/js/src/actions/forms.js
+++ web/hitparade/static/src/js/src/actions/forms.js
@@ -4,14 +4,10 @@ import _ from 'lodash';
  * Form validation
  */
 const validationFuncs = {
-  email: (value) => /^([a-z0-9+_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,24})$/.test(value),
-  passLength: (value) => /^[a-zA-Z0-9+_-]{6,18}$/.test(value),
   required: (value) => !!value,
 }
 
 const validationErrorMessages = {
-  email: 'Must be a valid email.',
-  passLength: 'Password must be at least 6 characters.',
   required: 'Field is required.',
 }
 
